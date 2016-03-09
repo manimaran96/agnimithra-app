84d7a3e69c6973e5e2eba16a28ca1973b0bad1ae
ragulkanth <ragul274@gmail.com>
2016-03-09 21:15:32 +0530

    DB and migrations are removed

--- cse/models.py
+++ cse/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-    id = models.AutoField(primary_key=True, default=000) 
+    id = models.AutoField(primary_key=True, default=1) 
     mailid = models.CharField(max_length=30, unique=True)
-    candidate = models.CharField(max_length=150)
+    candidate = models.CharField(max_length=50)
     college = models.CharField(max_length=150)
